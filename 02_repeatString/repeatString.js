170266e54ed38209d9e43bdcbd4146617340fcb6
georgetester <georgemtester@gmail.com>
2023-04-09 20:53:35 +0100

    passed helloWorld and repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
+
+const repeatString = function(string,num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 1; i <= num; i++) {
+        result += string;
+    }
+    console.log(result)
+    return result;
 };
+
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
